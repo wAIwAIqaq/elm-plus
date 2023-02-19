564406e4fcb0f243739f72f6e41004d60abb824a
wAiwAi <822458398@qq.com>
2023-02-19 22:50:36 +0800

    init

---
+++ elm-plus/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import compList from './components/comp-list.vue'
+</script>
+
+<template>
+  <compList />
+</template>
+
+<style scoped>
+</style>
---
+++ elm-plus/src/components/comp-list.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <p>list</p>
+</template>
+
+<style scoped>
+</style>
